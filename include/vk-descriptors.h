9b621fcc91719fc76e54f3a1fabb9720d4cf2a96
Sebastian Dawid <sdawid@techfak.uni-bielefeld.de>
2024-01-17 11:46:44 +0100

    Added growable descriptor set allocator.

--- include/vk-descriptors.h
+++ include/vk-descriptors.h
@@ -31,3 +31,26 @@ struct descriptor_allocator_t
 
     std::optional<vk::DescriptorSet> allocate(vk::Device device, vk::DescriptorSetLayout layout);
 };
+
+struct descriptor_allocator_growable_t
+{
+    struct pool_size_ratio_t
+    {
+        vk::DescriptorType type;
+        float ratio;
+    };
+
+    std::vector<pool_size_ratio_t> ratios;
+    std::vector<vk::DescriptorPool> full_pools;
+    std::vector<vk::DescriptorPool> ready_pools;
+    std::uint32_t sets_per_pool;
+
+    bool init(vk::Device device, std::uint32_t initial_sets, std::span<pool_size_ratio_t> pool_ratios);
+    void clear_pools(vk::Device device);
+    void destroy_pools(vk::Device device);
+
+    std::optional<vk::DescriptorSet> allocate(vk::Device device, vk::DescriptorSetLayout layout);
+
+    std::optional<vk::DescriptorPool> get_pool(vk::Device device);
+    std::optional<vk::DescriptorPool> create_pool(vk::Device device, std::uint32_t set_count, std::span<pool_size_ratio_t> pool_ratios);
+};
--- src/vk-descriptors.cpp
+++ src/vk-descriptors.cpp
@@ -68,3 +68,110 @@ std::optional<vk::DescriptorSet> descriptor_allocator_t::allocate(vk::Device dev
     }
     return sets[0];
 }
+
+bool descriptor_allocator_growable_t::init(vk::Device device, std::uint32_t max_sets, std::span<pool_size_ratio_t> pool_ratios)
+{
+    this->ratios.clear();
+    for (auto r : pool_ratios)
+        this->ratios.push_back(r);
+
+    auto ret = this->create_pool(device, max_sets, pool_ratios);
+    if (!ret.has_value()) return false;
+    this->sets_per_pool = max_sets * 1.5f;
+    this->ready_pools.push_back(ret.value());
+
+    return true;
+}
+
+void descriptor_allocator_growable_t::clear_pools(vk::Device device)
+{
+    for (auto p : this->ready_pools)
+    {
+        device.resetDescriptorPool(p);
+    }
+    for (auto p : this->full_pools)
+    {
+        device.resetDescriptorPool(p);
+        this->ready_pools.push_back(p);
+    }
+    this->full_pools.clear();
+}
+
+void descriptor_allocator_growable_t::destroy_pools(vk::Device device)
+{
+    for (auto p : this->ready_pools)
+    {
+        device.destroyDescriptorPool(p);
+    }
+    this->ready_pools.clear();
+    for (auto p : this->full_pools)
+    {
+        device.destroyDescriptorPool(p);
+    }
+    this->full_pools.clear();
+}
+
+std::optional<vk::DescriptorSet> descriptor_allocator_growable_t::allocate(vk::Device device, vk::DescriptorSetLayout layout)
+{
+    auto ret = this->get_pool(device);
+    if (!ret.has_value()) return std::nullopt;
+    vk::DescriptorPool pool = ret.value();
+    vk::DescriptorSetAllocateInfo alloc_info(pool, 1, &layout);
+
+    auto [result, set] = device.allocateDescriptorSets(alloc_info);
+    if (result == vk::Result::eErrorOutOfPoolMemory || result == vk::Result::eErrorFragmentedPool)
+    {
+        this->full_pools.push_back(pool);
+        auto ret = this->get_pool(device);
+        if (!ret.has_value()) return std::nullopt;
+        pool = ret.value();
+        alloc_info.descriptorPool = pool;
+
+        std::tie(result, set) = device.allocateDescriptorSets(alloc_info);
+        if (result != vk::Result::eSuccess)
+        {
+            fmt::print(stderr, "[ {} ]\tFailed to allocate descriptor set!\n", ERROR_FMT("ERROR"));
+            return std::nullopt;
+        }
+    }
+
+    this->ready_pools.push_back(pool);
+    return set[0];
+}
+
+std::optional<vk::DescriptorPool> descriptor_allocator_growable_t::get_pool(vk::Device device)
+{
+    vk::DescriptorPool new_pool;
+    if (this->ready_pools.size() != 0)
+    {
+        new_pool = this->ready_pools.back();
+        this->ready_pools.pop_back();
+    }
+    else
+    {
+        auto ret = this->create_pool(device, this->sets_per_pool, this->ratios);
+        if (!ret.has_value()) return std::nullopt;
+        new_pool = ret.value();
+        this->sets_per_pool = this->sets_per_pool * 1.5f;
+        if (this->sets_per_pool > 4092)
+            this->sets_per_pool = 4092;
+    }
+    return new_pool;
+}
+
+std::optional<vk::DescriptorPool> descriptor_allocator_growable_t::create_pool(vk::Device device, std::uint32_t set_count, std::span<pool_size_ratio_t> pool_ratios)
+{
+    std::vector<vk::DescriptorPoolSize> pool_sizes;
+    for (pool_size_ratio_t ratio : pool_ratios)
+    {
+        pool_sizes.push_back(vk::DescriptorPoolSize(ratio.type, std::uint32_t(ratio.ratio * set_count)));
+    }
+    vk::DescriptorPoolCreateInfo pool_info({}, set_count, pool_sizes);
+    auto [result, pool] = device.createDescriptorPool(pool_info);
+    if (result != vk::Result::eSuccess)
+    {
+        fmt::print(stderr, "[ {} ]\tFailed to create descriptor pool!\n", ERROR_FMT("ERROR"));
+        return std::nullopt;
+    }
+    return pool;
+}
